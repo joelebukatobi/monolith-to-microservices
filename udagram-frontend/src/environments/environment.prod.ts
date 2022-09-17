2e32667af9e0d36a2379a88377be08e10afc9763
joelebukatobi <joelebuka@gmail.com>
2022-09-17 15:25:27 +0100

    Edited Server.ts for both user and feed

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a40d7f90fd605450895be907a3c489f8-336675278.us-east-1.elb.amazonaws.com:8080/api/v0',
 };
 
 /*
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a03268e232005426fafc39dbebab7e83-145037532.us-east-1.elb.amazonaws.com:8080/api/v0',
 };
 
 /*
